a687da959d574ab6167f85c80758b72df8ad7e19
Goryudyuma <umaretekyoumade@gmail.com>
2014-10-01 13:28:45 +0900

    擬似言語との齟齬をなくしました

--- Insertion_sort.php
+++ Insertion_sort.php
@@ -1,10 +1,10 @@
 <?php
 	function Insertion_Sort_A($A){
-			for($j=2;$j<count($A);$j++){
+			for($j=1;$j<count($A);$j++){
 					$key=$A[$j];
 				//A[j]をソート列A[1..j-1]に挿入
 				$i=$j-1;
-				while($i>0&&$A[$i]>$key){
+				while($i>=0&&$A[$i]>$key){
 						$A[$i+1]=$A[$i];
 						$i=$i-1;
 				}

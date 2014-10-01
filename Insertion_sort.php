094ed9eb02535e036fccbc465867ab5fd5142c6a
Goryudyuma <umaretekyoumade@gmail.com>
2014-10-01 13:16:17 +0900

    Insertion_sortの実装

---
+++ Insertion_sort.php
@@ -0,0 +1,27 @@
+<?php
+	function Insertion_Sort_A($A){
+			for($j=2;$j<count($A);$j++){
+					$key=$A[$j];
+				//A[j]をソート列A[1..j-1]に挿入
+				$i=$j-1;
+				while($i>0&&$A[$i]>$key){
+						$A[$i+1]=$A[$i];
+						$i=$i-1;
+				}
+				$A[$i+1]=$key;
+			}
+			return $A;
+	}
+
+	$hairetsu=array();
+
+	for($i=0;$i<100;$i++){
+		array_push($hairetsu,rand());
+	}
+
+	print_r($hairetsu);
+
+	$hairetsu=Insertion_Sort_A($hairetsu);
+
+	print_r($hairetsu);
+	?>
